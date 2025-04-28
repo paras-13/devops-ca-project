32c0a13d604bcf1d3c1aa63fa6de74a3cba8f7b0
Paras Upadhyay <paras13upadhyay@gmail.com>
2025-04-29 00:19:04 +0530

    Navbar heading Changed commit 1

--- frontend/src/components/navBar/NavBar.jsx
+++ frontend/src/components/navBar/NavBar.jsx
@@ -20,7 +20,7 @@ const NavBar = () => {
     <div className="navbar">
       <div className="left">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span>Fasak</span>
+          <span>Varta</span>
         </Link>
         <HomeOutlinedIcon />
         {darkMode ? (
