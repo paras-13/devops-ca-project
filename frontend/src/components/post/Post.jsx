99eb2499aa8d96aa4a02de10236c87509ef88f34
Paras Upadhyay <paras13upadhyay@gmail.com>
2025-04-29 12:31:22 +0530

    Change in post UI commit 1

--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -83,7 +83,7 @@ const Post = ({ post }) => {
           </div>
           <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
             <TextsmsOutlinedIcon />
-            12 Comments
+            Comments
           </div>
           <div className="item">
             <ShareOutlinedIcon />
